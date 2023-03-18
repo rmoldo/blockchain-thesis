a5996a6aa95373ca1db92e85f45c2b30cedcec30
Radu Moldovan <raducons.moldovan@protonmail.ch>
2023-03-18 13:10:02 +0200

    Add key initialization and sign method
    Generate a pair of rsa public and private key.
    Create a sign function to be used to sign transactions

--- blockchain/wallet.go
+++ blockchain/wallet.go
@@ -1,13 +1,40 @@
 package blockchain
 
 import (
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
-	"encoding/base64"
 )
 
 type Wallet struct {
 	PrivateKey rsa.PrivateKey
-	PublicKey  []byte
+	PublicKey  rsa.PublicKey
+}
+
+func (w *Wallet) InitKeys() {
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 4096)
+	w.PrivateKey = *privateKey
+
+	w.PublicKey = privateKey.PublicKey
+}
+
+func (w *Wallet) Sign(data []byte) []byte {
+	// Hash the data
+	dataHash := sha256.New()
+	_, err := dataHash.Write(data)
+
+	if err != nil {
+		// TODO: error handling
+	}
+
+	dataHashSum := dataHash.Sum(nil)
+
+	signature, err := rsa.SignPSS(rand.Reader, &w.PrivateKey, crypto.SHA256, dataHashSum, nil)
+
+	if err != nil {
+		// TODO: error handling
+	}
+
+	return signature
 }
